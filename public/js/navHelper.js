4a79f36cc5354ddbb7c2079ff2c2d57c044c7f08
patterncoder <chris@oldtowndining.com>
2014-11-25 17:33:46 -0800

    added some more nav and Caslon Font

--- public/js/navHelper.js
+++ public/js/navHelper.js
@@ -1,10 +1,6 @@
 ﻿
 $(document).ready(function () {
-    //$('ul.nav > li').click(function (e) {
-    //    e.preventDefault();
-    //    $('ul.nav > li').removeClass('active');
-    //    $(this).addClass('active');
-    //});
 
     $('ul.nav > li > a[href="' + document.location.pathname + '"]').parent().addClass('active');
+
 });

8b8fdbf40964bcd0ecd3057d9b4fe0bc43f461fa
patterncoder <chris@oldtowndining.com>
2014-12-26 19:11:37 -0800

    fixed broken links

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,8 +6,23 @@ var controller = require('../controllers/myController');
 router.get('/', function(req, res) {
   res.render('index', { title: "Baily's" });
 });
+router.get('/dining', function (req, res) {
+    res.render('dining', {title: "Dining"});
+});
+router.get('/nightclub', function (req, res) {
+    res.render('nightclub', { title: "Nightclub" });
+});
+router.get('/music', function (req, res) {
+    res.render('music', { title: "Music" });
+});
 router.get('/banquets', function (req, res) {
-    res.render('banquets');
+    res.render('banquets', { title: "Banquets" });
+});
+router.get('/about', function (req, res) {
+    res.render('about', { title: "About" });
+});
+router.get('/about/history', function (req, res) {
+    res.render('history', { title: "History" });
 });
 router.get('/users', controller.SendMessage);
 

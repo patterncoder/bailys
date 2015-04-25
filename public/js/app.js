c09bc3a7c837ee940101a9c57335c60731fb51b4
patterncoder <chris@oldtowndining.com>
2015-04-24 20:34:20 -0700

    another quick commit for more email address work

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,10 @@ $('#signUpForm').submit(function (event) {
         url = $form.attr('action');
     var posting = $.post(url, { email: $('#email').val() });
     posting.done(function (data) {
+        if (data.success)
+        {
+            $('#signUpModalBody').html('Your email was successfully saved!')
+        }
         console.log(data);
     });
 });

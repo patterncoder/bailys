bc735071c5c461ca09b8aaad5446cf1c685ca673
patterncoder <chris@oldtowndining.com>
2015-07-17 17:20:54 -0700

    trying to get favicon working

--- server/config/express.js
+++ server/config/express.js
@@ -35,7 +35,7 @@ module.exports = function (app, config) {
     
     
     // uncomment after placing your favicon in /public
-    //app.use(favicon(__dirname + '/public/favicon.ico'));
+    app.use(favicon(__dirname + '/../../public/favicon.ico'));
     app.use(logger('dev'));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));

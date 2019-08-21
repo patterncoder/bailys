2b2f505b05dec9a520047e7953fcf484d7a7ea84
trillobite <jparnell0@protonmail.com>
2019-08-20 19:21:52 -0700

    mailerjs is no longer on an external server

--- server/routes/banquetSubmit.js
+++ server/routes/banquetSubmit.js
@@ -2,27 +2,43 @@ const express = require("express");
 const router = express.Router();
 const request = require("request");
 const bodyParser = require("body-parser");
+const mailer = require("../utils/mailer");
 
 
 
 const jsonParser = bodyParser.json();
 
 //POST for the inquiry form on banquets.
-router.post("/", jsonParser, (req, res) => {
+router.post("/", jsonParser, async (req, res) => {
 
     console.log(req.body);
 
     try {
-        console.log("sending request...");
-        request.post("http://67.205.162.241:1984/inquiry", { json: req.body }, (err, response, body) => {
-            //console.log(response);
-            //send the response from the url above back.
-            res.send(response.statusCode);
-        });
-
+        let tmp = req.body;
+        let message = `
+        Customer Name: ${tmp.name}
+        Customer Email: ${tmp.email}
+        Customer Number: ${tmp.phoneNumber}
+    
+        message: ${tmp.message}`;
+        let response = await mailer(message);
+    
+        res.send(response)
     } catch (e) {
         res.send(e);
     }
+
+    // try {
+    //     console.log("sending request...");
+    //     request.post("http://67.205.162.241:1984/inquiry", { json: req.body }, (err, response, body) => {
+    //         //console.log(response);
+    //         //send the response from the url above back.
+    //         res.send(response.statusCode);
+    //     });
+
+    // } catch (e) {
+    //     res.send(e);
+    // }
 });
 
 module.exports = router;
---
+++ server/utils/mailer.js
@@ -0,0 +1,64 @@
+/*
+	File: mailer.js
+	Auth: Jesse Parnell
+	Desc: Handles mailing of inquiries.
+*/
+
+const nodemailer = require("nodemailer");
+const Promise = require("promise");
+const fs = require("fs");
+const env = "./pass.env"; //path to the env file.
+
+//load up the env password to link gmail.
+const load = (path) => {
+	console.log("loading password");
+	let dfd = new Promise((resolve, reject) => {
+
+		fs.readFile(path, {encoding: "utf-8"}, (err, pass) => {
+			if(err) {
+				throw err;
+			}
+
+			resolve(pass);
+		});
+
+	});
+	
+
+	return dfd;
+};
+
+//handles the actual mailing process...
+const mailer = async (message) => {
+
+	let pass = await load(env); //get the password...
+	console.log("mailer pass:", pass);
+
+	const transporter = nodemailer.createTransport({
+		service: "Gmail",
+		auth: {
+			user: "bailys.ai.helper@gmail.com",
+			pass: pass, //password from env.
+		}
+	});
+
+	let mailOptions = {
+		to: "events@baily.com",
+		subject: "Baily.com Inquiry",
+		text: message,
+	};
+
+	let result = undefined;
+	await transporter.sendMail(mailOptions, (err, res) => {
+		if(err) {
+			result = err;
+		} else {
+			result = res.response;
+			console.log("Message sent:", res.response);
+		}
+	});
+
+	return result;
+};
+
+module.exports = mailer;

d6fb5ab3986a71be9505e4380f73402d233541fc
trillobite <jparnell0@protonmail.com>
2019-09-03 20:55:30 -0700

    the personal message now works from url data

--- public/js/urlParse.js
+++ public/js/urlParse.js
@@ -1,9 +1,12 @@
 
 let urlParse = (paramName) => {
     let url = document.URL;
-    let paramsSearch = new URLSearchParams(url.search);
-    console.log(url)
-    return paramsSearch.has(paramName) ? paramsSearch.get(paramName) : undefined;
+    let index = url.search(paramName);
+    index += paramName.length; //get the length of the param name.
+    index++; //need to also skip over the equals sign.
+
+    let strUrl = url.toString();
+    return unescape(strUrl.substring(index, strUrl.length));
 }
 
 let mkInvite = () => {
